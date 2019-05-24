86633f13eee334bb2f812e7deba09dcb777c1770
Victor Costa <victorcosta@jera.com.br>
2019-05-23 20:49:21 -0400

    Initial commit

---
+++ src/plugins/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB_24z8dppsfoLRnzg9y6pzczeMV4ixgNU',
+  authDomain: 'fir-chat-2ce78.firebaseapp.com',
+  databaseURL: 'https://fir-chat-2ce78.firebaseio.com',
+  projectId: 'fir-chat-2ce78',
+  storageBucket: 'fir-chat-2ce78.appspot.com',
+  messagingSenderId: '897322852181',
+  appId: '1:897322852181:web:6606dc7596dcf2ea'
+};
+
+firebase.initializeApp(firebaseConfig);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,150 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-subheader>
+      Tweets
+    </v-subheader>
+    <div v-for="(item, index) in reversedTweets">
+      <div class="tweet-container">
+        <div class="tweet-header">
+          <v-avatar size="50">
+            <img :src="item.user.avatar">
+          </v-avatar>
+          <span>
+            <strong>{{ item.user.name }}</strong>
+          </span>
+        </div>
+        <div class="tweet">
+          <p>
+            {{ item.tweet }}
+          </p>
+        </div>
+      </div>
+      <v-divider
+        v-if="!(index === tweets.length - 1)"
+      ></v-divider>
+    </div>
+    <div class="text-xs-right fixed-button">
+      <v-btn fab dark color="cyan" @click="openDialog">
+        <v-icon dark>add</v-icon>
+      </v-btn>
+    </div>
+    <v-layout row justify-center>
+      <v-dialog v-model="dialog" max-width="500">
+        <v-card>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm6 md4>
+                  <v-textarea
+                    label="O que vc tem a dizer?"
+                    v-model="newTweet"
+                    maxlength="170"
+                  ></v-textarea>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" flat @click="saveTweet">Postar</v-btn>
+            <v-btn color="green darken-1" flat @click="dialog = false">Descartar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import firebase from 'firebase';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
+  name: 'Home',
+  data() {
+    return {
+      fireDb: firebase.database(),
+      dialog: false,
+      newTweet: '',
+      tweets: [],
+    };
+  },
+  mounted() {
+    this.getAllTweets();
+  },
+  computed: {
+    reversedTweets() {
+      return this.tweets.reverse();
+    },
+  },
+  methods: {
+    getAllTweets() {
+      this.fireDb.ref('tweets').once('value').then((response) => {
+        if (response.val()) {
+          const keys = Object.keys(response.val())
+          keys.forEach((key) => {
+            let tweet = response.val()[key];
+            tweet.id = key;
+            this.tweets.push(tweet);
+          })
+        }
+      });
+    },
+    onTweetAdded() {
+      this.fireDb.ref('tweets').on('child_added', (response) => {
+        if (!this.tweets.some(tweet => tweet.id === response.key)) {
+          console.log('asdasdas')
+          const newTweet = response.val();
+          newTweet.id = response.key;
+          this.tweets.unshift(newTweet);
+        }
+      });
+    },
+    openDialog() {
+      this.dialog = true;
+    },
+    saveTweet() {
+      this.dialog = false;
+      this.fireDb.ref('tweets').push({
+        tweet: this.newTweet,
+        user: {
+          name: 'Victor',
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg'
+        },
+      });
+      this.newTweet = '';
+      this.onTweetAdded();
+    },
   },
 };
 </script>
+<style lang="scss">
+.fixed-button {
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+  margin: 0 10px 10px
+}
+.tweet-container {
+  margin-left: 8px;
+  margin-top: 4px;
+  display: flex;
+
+  .tweet-header {
+    span {
+      position: absolute;
+      margin-left: 8px;
+      font-weight: bold;
+      color: #14171a;
+    }
+  }
+
+  .tweet {
+    margin-top: 28px;
+    margin-left: 8px;
+    p {
+      width: 100%;
+      word-break: break-all;
+    }
+  }
+
+}
+</style>
